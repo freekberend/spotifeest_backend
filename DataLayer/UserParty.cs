902829cfd305d1f11270872a3328a046282f431f
OlafPl <O.plaatsman@gmail.com>
2022-12-14 11:30:37 +0100

    derdeaanpassing

---
+++ DataLayer/UserParty.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLayer
+{
+    internal class UserParty
+    {
+        public User user { get; set; }
+        public Party party { get; set; }
+    }
+}
