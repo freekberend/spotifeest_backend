f17879e5753059d1988d0dce2d787d45f1221821
OlafPl <O.plaatsman@gmail.com>
2022-12-14 10:37:17 +0100

    KlasseAanpassingen

--- DataLayer/Preference.cs
+++ DataLayer/Preference.cs
@@ -11,9 +11,12 @@ namespace DataLayer
         public int Id { get; set; }
 
         public Party Feest { get; set; }
-        public string Genre1 { get; set; }
-        public string Genre2 { get; set;}
-        public string Genre3 { get; set;}
+        public string Nummer1 { get; set; }
+        public string Nummer2 { get; set;}
+        public string Nummer3 { get; set;}
+        public string Oorsprong1 { get; set; }
+        public string Oorsprong2 { get; set; }
+        public string Oorsprong3 { get; set; }
 
     }
 }
